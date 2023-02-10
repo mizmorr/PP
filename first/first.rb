0990fe77f15adfe20d92095a3236b49c6b220c76
mizmor <mizmorrr@gmail.com>
2023-02-11 02:40:32 +0300

    mv

--- first/first.rb
+++ first/first.rb
@@ -1,1 +1,18 @@
-puts "Hello world!"
+puts 'Hello world!'
+puts "Hello #{`whoami`}"
+puts 'whats lang'
+str = gets.chomp
+case str 
+    when "ruby"
+        puts "sneaky"
+    when "c"
+        puts "respect"
+    else
+        puts 'dunno'
+    end
+puts 'command'
+str = gets.chomp
+system "ruby -e \'#{str}\'"
+puts 'another command'
+str = gets.chomp
+system "ruby -e \'#{str}\'"
