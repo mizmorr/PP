6bf0f2eac71b50791d2383290518d87856608bf7
mizmor <mizmorrr@gmail.com>
2023-02-11 02:40:44 +0300

    letsgo2

---
+++ first/second.rb
@@ -0,0 +1,34 @@
+# def test 
+#     (1..10).each do |i|
+#         yield i
+#     end
+# end
+
+# test {|x| puts x**3}
+
+
+def gcd(x,y)
+    return x if y == 0
+
+    cur = (x%y)
+
+    return gcd(y,cur)
+end
+
+
+# def number(x)
+#     res = 0
+#     0.upto(x) do |i|
+#         if gcd(x,i)!=1 then res+=1
+#     end
+#     return res
+# end
+def number(x)
+    res=0
+    0.upto(x-1) do |i|
+        puts i if gcd(x,i)!=1 and i%2==0
+    end
+    return res
+end
+
+
