3c149b91ad18c2a3d254593cedd496308bc0bc01
mizmor <mizmorrr@gmail.com>
2023-02-17 23:41:12 +0300

    first done

--- first/fourth/first.rb
+++ first/fourth/first.rb
@@ -1,7 +1,6 @@
-def func(first, second)
+def same_value_num(first, second)
     return (first - (first - second)).length
 end
 
-first = [1,2,-3,5,7,8]
-second = [2,-3,5,10,12]
-puts func first,second
+
+
