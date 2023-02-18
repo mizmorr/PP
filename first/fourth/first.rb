991304a39a09f59a44363a31a893a687856b2bf5
mizmor <mizmorrr@gmail.com>
2023-02-18 09:24:14 +0300

    fix +add yield

--- first/fourth/first.rb
+++ first/fourth/first.rb
@@ -5,3 +5,11 @@ end
 def same_value_num2(first, second)
     return first&second.length
 end
+
+def same_value_num3(first, second)
+     return yield first, second
+end
+    
+a1=[1,2,3,5,6]
+a2=[1,2,3]
+p same_value_num3(a1,a2) {|ar1,ar2| ar1&ar2} 
--- first/fourth/summary.rb
+++ first/fourth/summary.rb
@@ -30,7 +30,7 @@ def func_one_array(num, file_path)
         when 2
             puts  min_count file_data
         when 3
-            puts slice_values file_data
+            puts slice_values 1,5, file_data
         when 4
             puts negative_positive file_data
         when 5
@@ -39,6 +39,26 @@ def func_one_array(num, file_path)
             puts 'bad type'
         end
 end
+
+def proccesing_array(array)
+    yield array
+end
+
+def func_one_array_yeild(num,file_path)
+    file = File.open(file_path)
+    file_data = file.read.split().map(&:to_i)
+    case num
+        when 2
+            puts  processing_array(file_data) {|ar| ar.slice(left,rigth).select{|e| e==ar.min}.length}
+        when 3
+            puts processing_array(1,5,file_data) {|arr| arr.slice(left,rigth).uniq}
+        when 4
+            puts processing_array(file_data) {|ar| ar.select{|elem| elem>=0}.concat ar.select{|elem| elem<0}}
+        when 5
+            puts processing_array(file_data) {|array| array.find_all{|elem| array.find_all{|e| e!=elem}.combination(2).to_a.map{|el| el.sum}.include?(elem)}}
+        else 
+            puts 'bad type'
+        end
 
 def func_2array(first, second)
     file = File.open(file_path)
