abc8d1d85e2f87e4a6e4f3bf2277282e2c27346a
mizmorr <mizmorrr@gmail.com>
2023-02-25 12:32:55 +0300

    snake_case

--- first/second/second.rb
+++ first/second/second.rb
@@ -1,15 +1,13 @@
-def maxDigit(val,max)
+def get_max_digit_tail(val,max)
     return max if val==0
     if val%10>max and (val%10)%3!=0 
         current_max = val%10
     else 
         current_max = max
     end
-    return maxDigit(val/10,current_max)
+    return get_max_digit(val/10,current_max)
 end
 
-def mDig(val)
-    return maxDigit(val,-1)
+def max_dig(val)
+    return get_max_digit_tail(val,-1)
 end
-# k = maxDigit 137, 0
-puts mDig 3479
