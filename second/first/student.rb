c79291fea5ad353e6161c5a0fac8598783b2d93d
mizmorr <mizmorrr@gmail.com>
2023-03-11 00:25:38 +0300

    3-5 done

--- second/first/student.rb
+++ second/first/student.rb
@@ -1,44 +1,21 @@
 class Student
-    def initialize(name,id,last_name,git,phone,telegram)
-        @Name,@ID,@Last_name,@Git,@Phone,@Telegram=name,id,last_name,git,phone,telegram
-    end
-    def ID 
-        @ID
-    end
-    def ID=(id)
-        @ID = id
-    end
-    def Name
-        @Name
-    end
-    def Name=(name) 
-        @Name=name
-    end
-    def Last_name=(last_name)
-        @Last_name=last_name
-    end
-    def Last_name
-        @Last_name
-    end
-    def Git
-        @Git
-    end
-    def Git=(git)
-        @Git=git
-    end
-    def Telegram
-        @Telegram
-    end
-    def Telegram=(telegram)
-        @Telegram=telegram
-    end
-    def Phone
-        @Phone
-    end
-    def Phone=(phone)
-        @Phone=phone
-    end
+    attr_accessor :ID,
+                  :Name,
+                  :Last_name,
+                  :Git,
+                  :Phone,
+                  :Telegram
+                  :Email
 
+    def initialize(name:"John",last_name:"Doe",options={})
+        @Name,@Last_name=name,last_name
+        self.ID=options[:ID]
+        self.Git=options[:Git]
+        self.Phone=options[:Phone]
+        self.Email=options[:Email]
+        self.Telegram=options[:Telegram]
+    end
+   
     def print_student()
         puts "Name: #{@Name}, Last name: #{@Last_name}, Git: #{@Git}, Telegram: #{@Telegram}"
     end
