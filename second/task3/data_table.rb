51e486e1f69faac03c6b24c3cd3003296cf13471
mizmorr <safonov.ilya22@gmail.com>
2023-04-04 01:53:30 +0300

    task2.3.1

---
+++ second/task3/data_table.rb
@@ -0,0 +1,14 @@
+class Data_table
+    
+    def initialize(data:)
+        self.data = data
+    end
+    
+    private 
+    def data
+        @data
+    end
+    def data=(data)
+        @data = data
+    end
+end
