4fd21051b9d748e4d69e30fb92e3f29d6c66a328
mizmorr <safonov.ilya22@gmail.com>
2023-04-04 02:04:02 +0300

    task2.3.2

--- second/task3/data_table.rb
+++ second/task3/data_table.rb
@@ -3,7 +3,17 @@ class Data_table
     def initialize(data:)
         self.data = data
     end
-    
+
+    def at(row:, column:)
+        data[row][column]
+    end
+
+    def rows_count
+        data.length
+    end
+    def columns_count
+        data[0].length
+    end
     private 
     def data
         @data
