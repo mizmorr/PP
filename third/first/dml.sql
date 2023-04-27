871257e3e6604ec6f0b7923b42e6e49c006a6ab0
mizmorr <mizmorrr@gmail.com>
2023-04-28 02:21:46 +0300

    task3.1.4

---
+++ third/first/dml.sql
@@ -0,0 +1,4 @@
+insert into Student(name,last_name,git,telegram,email,phone) values 
+('John','Doe','joedoe','doejoe','joedoejoe@gmail.com','89607654312'),
+('Ann','Doe','anndoe','doeann','ann123@gmail.com','89602345678'),
+('Unknown','Doe',NULL,'unknown_@',NULL,NULL);
